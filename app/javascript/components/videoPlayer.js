0662fd7966f3b3fadaa900a2bcc7e501236a6bc8
Djamal Sadaghiani <dj8amal@gmail.com>
2018-08-09 16:03:53 +0800

    finished the video player

---
+++ app/javascript/components/videoPlayer.js
@@ -0,0 +1,7 @@
+const activateVideo = function {
+
+}
+
+const renderSteps = function {
+
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'tutorials/index'
-  get 'tutorials/show'
-  get 'tutorials/destroy'
-  get 'tutorials/new'
-  get 'tutorials/create'
-  get 'tutorials/edit'
-  get 'tutorials/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :companies
+  resources :companies do
+    resources :tutorials
+  end
 end
