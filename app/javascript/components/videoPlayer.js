5a9fbf80aaed76ed70b469a298bd6b103a6ce5c7
Djamal Sadaghiani <dj8amal@gmail.com>
2018-08-10 13:36:17 +0800

    added the autoscroll next to the video

--- app/javascript/components/videoPlayer.js
+++ app/javascript/components/videoPlayer.js
@@ -1,14 +1,52 @@
-const activateVideo = function {
+import videojs from "video.js";
+
+const activateVideo = function () {
   videojs(document.querySelector('.video-js')).ready(function () {
     this.on('timeupdate', function () {
-      console.log(this.currentTime());
       renderSteps();
     })
   });
 
 }
 
-const renderSteps = function {
+const startTimes = function () {
+  const times = [];
+  document.querySelectorAll(".step").forEach((step) => {
+  times.push(step.dataset.time);
+  });
+  return times;
+}
+
+const renderSteps = function () {
   const player = videojs(document.querySelector('.video-js'))
   console.log(player.currentTime());
+  const playerTime = player.currentTime();
+  const times = startTimes();
+  for (let i = 0; i < times.length; i++) {
+    if (Math.round(playerTime) == times[i]) {
+      console.log(`#step-${i+1}`);
+      moveStep(`#step-${i+1}`);
+      break;
+    }
+  }
 }
+
+const moveStep = function (step) {
+  $('.steps-area').animate({
+      scrollTop: $(step).parent().scrollTop() + $(step).offset().top - $(step).parent().offset().top
+  }, {
+      duration: 1000,
+      specialEasing: {
+          width: 'linear',
+          height: 'easeOutBounce'
+      },
+      complete: function (e) {
+        console.log("animation completed");
+    }
+  });
+}
+
+export { moveStep };
+export { activateVideo };
+export { startTimes };
+export { renderSteps };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,12 @@
 import "bootstrap";
-import "video.js";
+import { activateVideo } from "../components/videoPlayer";
+import { startTimes } from "../components/videoPlayer";
+import { renderSteps } from "../components/videoPlayer";
+import { moveStep } from "../components/videoPlayer";
+
+const videoPlayer = document.querySelectorAll('.video-js');
+if (videoPlayer) {
+  //startTimes();
+  activateVideo();
+  //renderSteps();
+}
