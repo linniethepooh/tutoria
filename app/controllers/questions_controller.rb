bb4d1236328db2ae3bf7f07775722f4a80e73878
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-11 15:49:40 +0800

    added questions form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,17 @@
 class QuestionsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  skip_before_action :authenticate_user!, only: [:show, :new, :create]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def new
     @question = Question.new
-    @user = current_user
+    authorize @question
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.save
+    authorize @question
+    raise
   end
 
   def show
---
+++ app/policies/question_policy.rb
@@ -0,0 +1,17 @@
+class QuestionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  private
+end
