c1bf4ab20bf728a5b34d5c389392aa43ae0f846b
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-11 12:45:56 +0800

    working on questions

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,5 +1,9 @@
 class Users::InvitationsController < Devise::InvitationsController
-
+  def new
+    if current_user.status != "manager"
+      redirect_to dashboard_path, alert: "Not authorized"
+    end
+  end
   def create
     self.resource = invite_resource {|resource| resource.company = current_user.company }
   end
