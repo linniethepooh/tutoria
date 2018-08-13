97e7dd0b0a302240386ea7cbb10dcb2d9f845a66
Lindelwe Ncube <linniencube@gmail.com>
2018-08-13 13:49:33 +0800

    added new navbar and basic home page

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -5,7 +5,9 @@ const typedjs = () => {
   if (element != null) {
     var options = {
     strings: ["Instructions for the digitial age", "Instructions for the digitial age..."],
-    typeSpeed: 100
+    typeSpeed: 100,
+    loop: false,
+
     }
 
     var typed = new Typed(element, options);
