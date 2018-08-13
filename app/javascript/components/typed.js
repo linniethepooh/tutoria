4ab11247a86c7dda40048f4e3c38a35f1cac909d
Lindelwe Ncube <linniencube@gmail.com>
2018-08-13 10:51:56 +0800

    landing page WIP

---
+++ app/javascript/components/typed.js
@@ -0,0 +1,15 @@
+import Typed from 'typed.js';
+
+const typedjs = () => {
+ let element = document.querySelector(".element")
+  if (element != null) {
+    var options = {
+    strings: ["Instructions for the digitial age", "Instructions for the digitial age..."],
+    typeSpeed: 100
+    }
+
+    var typed = new Typed(element, options);
+  }
+}
+
+export {typedjs}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,11 @@ import { startTimes } from "../components/videoPlayer";
 import { renderSteps } from "../components/videoPlayer";
 import { moveStep } from "../components/videoPlayer";
 import { seekSteps } from "../components/videoPlayer";
+import { typedjs} from "../components/typed";
 
 const videoPlayer = document.querySelectorAll('.video-js');
-if (videoPlayer) {
+if (videoPlayer.length != 0) {
   activateVideo();
 }
+
+typedjs()
