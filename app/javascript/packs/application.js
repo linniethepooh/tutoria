991ad93d04b4266bc77ec66075c74035ee063cc3
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-14 14:13:02 +0800

    added functionalities to video player

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,13 @@ import { startTimes } from "../components/videoPlayer";
 import { renderSteps } from "../components/videoPlayer";
 import { moveStep } from "../components/videoPlayer";
 import { seekSteps } from "../components/videoPlayer";
+import { playback } from "../components/videoPlayer";
 import { typedjs} from "../components/typed";
 import { activateButton } from "../components/collapse";
 import { questionStep } from "../components/question_form";
 import { answerQuestion } from "../components/answer_form";
 import { activateParticles} from "../components/particles";
+import { setLoopMode } from "../components/videoPlayer";
 
 const videoPlayer = document.querySelectorAll('.video-js');
 
@@ -18,11 +20,11 @@ if (videoPlayer.length != 0) {
   activateButton();
 };
 
-
-
 const tutorialPage = document.querySelector('.tutorials.show');
 if (tutorialPage) {
   questionStep();
+  playback();
+  setLoopMode();
 };
 
 const dashboard = document.querySelector('.users.dashboard');
