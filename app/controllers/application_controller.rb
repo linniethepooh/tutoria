270a77a6e7b140cb47d05533f0bdf0c3f81c9e33
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-17 13:02:33 +0800

    started working on meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,10 @@ class ApplicationController < ActionController::Base
       end
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
